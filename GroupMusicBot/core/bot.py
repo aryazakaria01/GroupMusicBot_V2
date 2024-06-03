1b135c5aa7668873476aa55fc3fe7703c3b90c60
aryazakaria01 <ardianrazak7107@gmail.com>
2024-06-03 13:11:57 +0700

    fix syntax

--- GroupMusicBot/core/bot.py
+++ GroupMusicBot/core/bot.py
@@ -11,7 +11,7 @@ class GMB(Client):
         LOGGER(__name__).info("Starting Bot...")
         super().__init__(
             name="GroupMusicBot",
-            in_memory=True
+            in_memory=True,
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
