a38722cc6c8b1077bf912c6888116fba4ffdb33d
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-07 23:48:26 +0700

    none

--- GroupMusicBot/logging.py
+++ GroupMusicBot/logging.py
@@ -2,17 +2,18 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[%(asctime)s: %(levelname)-7s] %(name)-15s - %(message)s",
+    format="[ %(asctime)s: %(levelname)-6s ] %(name)-15s - %(message)s",
     datefmt="%H:%M:%S",
     handlers=[
         logging.FileHandler("MusicLogs.txt"),
         logging.StreamHandler(),
     ],
 )
-logging.getLogger("asyncio").setLevel(logging.INFO)
+logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pymongo").setLevel(logging.WARNING)
-logging.getLogger("pytgcalls").setLevel(logging.WARNING)
+logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("pymongo").setLevel(logging.INFO)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
