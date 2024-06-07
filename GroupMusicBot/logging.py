d582d910641f9b9e53feed863f6fadd87e3af83b
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-07 23:21:07 +0700

    fix logging

--- GroupMusicBot/logging.py
+++ GroupMusicBot/logging.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[ %(asctime)s: %(levelname)-8s ] %(name)-15s - %(message)s",
+    format="[%(asctime)s: %(levelname)-8s]%(name)-15s - %(message)s",
     datefmt="%H:%M:%S",
     handlers=[
         logging.FileHandler("MusicLogs.txt"),
