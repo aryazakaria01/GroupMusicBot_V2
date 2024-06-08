62f281fed5d61172936b3d5f09233d0a624f3904
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-09 03:41:32 +0700

    refactor

--- GroupMusicBot/plugins/tools/bug.py
+++ GroupMusicBot/plugins/tools/bug.py
@@ -13,52 +13,48 @@ from config import COMMAND_PREFIXES as cmd, LOG_GROUP_ID
 
 def get_text(message: Message) -> Optional[str]:
     text_to_return = message.text
-    if message.text is None:
+    if not text_to_return:
         return None
-    if " " not in text_to_return:
-        return None
-    try:
-        return message.text.split(None, 1)[1]
-    except IndexError:
-        return None
+    return " ".join(message.command[1:])
 
 
-@app1.on_message(filters.regex(f"^{cmd}bug"))
+@app1.on_message(filters.command("bug", prefixes=cmd))
 async def bug(_, message: Message):
     chat_id = message.chat.id
-    memek = await app.export_chat_invite_link(message.chat.id)
+    memek = await app1.export_chat_invite_link(message.chat.id)
     bugnya = get_text(message)
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     mention = f"[{user_name}](tg://user?id={str(user_id)})"
+
     if not bugnya:
-        await app.send_message(
+        await app1.send_message(
             chat_id,
-            text=_["bug_spt"],
+            text="Please provide a description of the bug.",
             disable_web_page_preview=True,
         )
         return
     else:
-        await app.send_message(
+        await app1.send_message(
             chat_id,
-            text=_["bug_sended"].format(bugnya),
+            text="Bug report sent! Thank you for your feedback.",
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton(_["CLOSE_BUTTON"], callback_data="cls")]]
+                [[InlineKeyboardButton("Close", callback_data="cls")]]
             ),
         )
-        await app.send_message(
+        await app1.send_message(
             LOG_GROUP_ID,
-            "📣 New bug reporting\n\n"
+            f"📣 New bug report\n\n"
             f"**Chat:** [{message.chat.title}]({memek})\n"
             f"**Name:** {mention}\n"
             f"**User ID:** {message.from_user.id}\n"
             f"**Username:** @{message.from_user.username}\n\n"
-            f"**Contents of the report:** {bugnya}",
+            f"**Bug Report:** {bugnya}",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton(_["bug_btn"], url=f"{message.link}")],
-                    [InlineKeyboardButton(_["CLOSE_BUTTON"], callback_data="cls")],
+                    [InlineKeyboardButton("View Message", url=f"{message.link}")],
+                    [InlineKeyboardButton("Close", callback_data="cls")],
                 ]
             ),
         )
