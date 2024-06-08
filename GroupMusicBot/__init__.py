0b9d86409a9c8ba8577dac8ebcc473486ee13f5a
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-09 03:34:19 +0700

    none

--- GroupMusicBot/__init__.py
+++ GroupMusicBot/__init__.py
@@ -1,3 +1,5 @@
+from config import API_ID, API_HASH, BOT_TOKEN
+
 from GroupMusicBot.core.bot import GMB
 from GroupMusicBot.core.git import git
 from GroupMusicBot.core.dir import dirr
@@ -12,7 +14,13 @@ heroku()
 
 app = GMB()
 userbot = Userbot()
-
+app1 = Client(
+    name="Gcp",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    in_memory=True,
+)
 
 from .platforms import *
 
