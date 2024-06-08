7997a7740b3e29029c98973ea171c2c6d7a79be6
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-09 02:58:26 +0700

    fix TypeError

--- GroupMusicBot/plugins/tools/dev.py
+++ GroupMusicBot/plugins/tools/dev.py
@@ -153,8 +153,10 @@ async def forceclose_command(_, CallbackQuery):
 )
 async def shellrunner(_, message: Message):
     if len(message.command) < 2:
-        return await edit_or_reply(message, text="<b>Example :</b>\n/sh git pull")
+        return await message.edit("<b>Example :</b>\n/sh git pull")
+
     text = message.text.split(None, 1)[1]
+
     if "\n" in text:
         code = text.split("\n")
         output = ""
@@ -166,11 +168,12 @@ async def shellrunner(_, message: Message):
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
                 )
+                out, _ = process.communicate()
+                output += f"<b>{x}</b>\n"
+                output += out.decode("utf-8")
+                output += "\n"
             except Exception as err:
-                await edit_or_reply(message, text=f"<b>ERROR :</b>\n<pre>{err}</pre>")
-            output += f"<b>{code}</b>\n"
-            output += process.stdout.read()[:-1].decode("utf-8")
-            output += "\n"
+                return await message.edit(text=f"<b>ERROR :</b>\n<pre>{err}</pre>")
     else:
         shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
         for a in range(len(shell)):
@@ -181,20 +184,17 @@ async def shellrunner(_, message: Message):
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
+            out, _ = process.communicate()
+            output = out.decode("utf-8")
         except Exception as err:
             print(err)
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            errors = traceback.format_exception(
-                etype=exc_type,
-                value=exc_obj,
-                tb=exc_tb,
-            )
-            return await edit_or_reply(
-                message, text=f"<b>ERROR :</b>\n<pre>{''.join(errors)}</pre>"
-            )
-        output = process.stdout.read()[:-1].decode("utf-8")
-    if str(output) == "\n":
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            errors = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            return await message.edit(text=f"<b>ERROR :</b>\n<pre>{''.join(errors)}</pre>")
+
+    if not output.strip():
         output = None
+
     if output:
         if len(output) > 4096:
             with open("output.txt", "w+") as file:
@@ -202,11 +202,12 @@ async def shellrunner(_, message: Message):
             await app.send_document(
                 message.chat.id,
                 "output.txt",
-                reply_to_message_id=message.id,
+                reply_to_message_id=message.message_id,
                 caption="<code>Output</code>",
             )
             return os.remove("output.txt")
-        await edit_or_reply(message, text=f"<b>OUTPUT :</b>\n<pre>{output}</pre>")
+        await message.edit(text=f"<b>OUTPUT :</b>\n<pre>{output}</pre>")
     else:
-        await edit_or_reply(message, text="<b>OUTPUT :</b>\n<code>None</code>")
+        await message.edit(text="<b>OUTPUT :</b>\n<code>None</code>")
+
     await message.stop_propagation()
