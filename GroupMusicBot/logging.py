08a315ec0a19a50e6c4bca6b4ce38fdbf4e58c1f
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-14 23:00:02 +0700

    revert

--- GroupMusicBot/logging.py
+++ GroupMusicBot/logging.py
@@ -1,25 +1,24 @@
 import logging
-from logging.handlers import RotatingFileHandler
-
-from config import LOG_FILE_NAME
 
 logging.basicConfig(
     level=logging.INFO,
     format="[ %(asctime)s: %(levelname)s ] %(name)-15s - %(message)s",
     datefmt="%H:%M:%S",
     handlers=[
-        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.FileHandler("MusicLogs.txt"),
         logging.StreamHandler(),
     ],
 )
+logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("pymongo").setLevel(logging.INFO)
+logging.getLogger("asyncio").setLevel(logging.WARNING)
 logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
 logging.getLogger("pyrogram.syncer").setLevel(logging.CRITICAL)
 logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
 logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
-LOGS = logging.getLogger(__name__)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
