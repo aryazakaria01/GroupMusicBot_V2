0f599dc9e4bce66690cee66338819fecacdf0e45
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-25 14:21:10 +0700

    redesign sudo

--- GroupMusicBot/misc.py
+++ GroupMusicBot/misc.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 from GroupMusicBot.logging import LOGGER
 from GroupMusicBot.core.mongo import mongodb
 
-SUDOERS = filters.user()
+SUDOERS = set(config.SUDO_USERS)
 
 HAPP = None
 _boot_ = time.time()
@@ -30,7 +30,7 @@ XCB = [
     "https",
     str(config.HEROKU_APP_NAME),
     "HEAD",
-    "master",
+    "main",
 ]
 
 
@@ -42,12 +42,13 @@ def dbb():
 
 async def sudo():
     global SUDOERS
-    SUDOERS.add(config.OWNER_ID)
+    LOGGER(__name__).info("Loading Sudo User Data")
+    SUDOERS.add(config.SUDO_USERS)
     sudoersdb = mongodb.sudoers
     sudoers = await sudoersdb.find_one({"sudo": "sudo"})
     sudoers = [] if not sudoers else sudoers["sudoers"]
-    if config.OWNER_ID not in sudoers:
-        sudoers.append(config.OWNER_ID)
+    if config.SUDO_USERS not in sudoers:
+        sudoers.append(config.SUDO_USERS)
         await sudoersdb.update_one(
             {"sudo": "sudo"},
             {"$set": {"sudoers": sudoers}},
@@ -56,7 +57,7 @@ async def sudo():
     if sudoers:
         for user_id in sudoers:
             SUDOERS.add(user_id)
-    LOGGER(__name__).info(f"Sudoers loaded.")
+    LOGGER(__name__).info(f"Loaded Sudo Users Successfully.")
 
 
 def heroku():
