ef89757e474015fd5e426430cda08e4a984b25fe
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-26 02:39:39 +0700

    revert

--- GroupMusicBot/core/mongo.py
+++ GroupMusicBot/core/mongo.py
@@ -1,12 +1,12 @@
 import sys
-import config
 from motor.motor_asyncio import AsyncIOMotorClient
 
+from config import MONGO_DB_URI
 from GroupMusicBot.logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
-    _mongo_async_ = AsyncIOMotorClient(config.MONGO_DB_URI)
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
     mongodb = _mongo_async_.GroupMusicBot
     LOGGER(__name__).info("Connected to your Mongo Database.")
 except:
