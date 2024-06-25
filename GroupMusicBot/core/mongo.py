8c7025a034a0e642891f15c1ce5fb143cadda4f6
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2024-06-26 02:52:08 +0700

    try

--- GroupMusicBot/core/mongo.py
+++ GroupMusicBot/core/mongo.py
@@ -1,4 +1,7 @@
 import sys
+
+from pymongo import MongoClient
+from pymongo.errors import PyMongoError
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_DB_URI
@@ -6,9 +9,9 @@ from GroupMusicBot.logging import LOGGER
 
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
-    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    _mongo_async_ = MongoClient(MONGO_DB_URI)
     mongodb = _mongo_async_.GroupMusicBot
     LOGGER(__name__).info("Connected to your Mongo Database.")
-except:
-    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+except PyMongoError as e:
+    LOGGER(__name__).error(f"Failed to connect to your Mongo Database, due to {e}")
     sys.exit()
